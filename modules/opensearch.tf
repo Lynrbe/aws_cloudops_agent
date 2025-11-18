d846821b62c60c728fe98899c44e53815b818086
FIXED-TERM Doan Thi Khanh Linh (MS/EDS32-XC) <fixed-term.Linh.DoanThiKhanh@vn.bosch.com>
2025-11-18 10:54:43 +0700

    update opensearch template #3

--- modules/opensearch.tf
+++ modules/opensearch.tf
@@ -1,89 +1,80 @@
-# Encryption Security Policy
-resource "aws_opensearchserverless_security_policy" "encryption" {
-  name = "${lower(var.project)}-encryption-policy"
+resource "aws_opensearchserverless_security_policy" "encryption_policy" {
+  name = "${var.project}-kb-encryption-policy"
   type = "encryption"
-
+  
+  # Using key default of AWS (AWSOwnedKey)
   policy = jsonencode({
-    Rules = [
+    "Rules" : [
       {
-        ResourceType = "collection"
-        Resource     = ["collection/${lower(var.project)}-collection"]
+        "ResourceType" : "collection",
+        "Resource" : ["collection/${var.project}-kb-collection"] #static name
       }
-    ]
-    AWSOwnedKey = true
+    ],
+    "AWSOwnedKey" : true 
   })
 }
 
-# Network Security Policy
-resource "aws_opensearchserverless_security_policy" "network" {
-  name = "${lower(var.project)}-network-policy"
+resource "aws_opensearchserverless_security_policy" "network_policy" {
+  name = "${var.project}-kb-network-policy"
   type = "network"
-
+  
   policy = jsonencode([
     {
-      Rules = [
+      "Rules" : [
         {
-          ResourceType = "collection"
-          Resource     = ["collection/${lower(var.project)}-collection"]
+          "ResourceType" : "collection",
+          "Resource" : ["collection/${var.project}-kb-collection"]
         }
-      ]
-      AllowFromPublic = true
+      ],
+      "AllowFromPublic" : true 
     }
   ])
 }
 
-# OpenSearch Serverless Collection
-resource "aws_opensearchserverless_collection" "rag" {
-  name = "${lower(var.project)}-collection"
+# 3. OpenSearch Serverless Collection
+resource "aws_opensearchserverless_collection" "rag_collection" {
+  name = "${var.project}-kb-collection"
   type = "VECTORSEARCH"
-
+  
   depends_on = [
-    aws_opensearchserverless_security_policy.encryption,
-    aws_opensearchserverless_security_policy.network
+    aws_opensearchserverless_security_policy.encryption_policy,
+    aws_opensearchserverless_security_policy.network_policy
   ]
-
-  tags = {
-    Name    = "${var.project}-Collection"
-    Project = var.project
-  }
 }
 
-# Data Access Policy
-resource "aws_opensearchserverless_access_policy" "data_access" {
-  name = "${lower(var.project)}-data-access-policy"
+# 4. Access Policy 
+resource "aws_opensearchserverless_access_policy" "rag_data_access" {
+  name = "${var.project}-kb-access"
   type = "data"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = [
+          aws_iam_role.kb_service_role.arn
+        ]
+        Action = [
+          "aoss:CreateIndex",
+          "aoss:DescribeIndex",
+          "aoss:UpdateIndex",
+          "aoss:DeleteIndex",
+          "aoss:ReadDocument",
+          "aoss:WriteDocument",
+          "aoss:DescribeCollection"
+        ]
+        Resource = [
+          aws_opensearchserverless_collection.rag_collection.arn,
+          "${aws_opensearchserverless_collection.rag_collection.arn}/*"
+        ]
+      },
+    ]
+  })
 
-  policy = jsonencode([
-    {
-      Rules = [
-        {
-          ResourceType = "index"
-          Resource     = ["index/${lower(var.project)}-collection/*"]
-          Permission = [
-            "aoss:CreateIndex",
-            "aoss:DescribeIndex",
-            "aoss:UpdateIndex",
-            "aoss:DeleteIndex",
-            "aoss:ReadDocument",
-            "aoss:WriteDocument"
-          ]
-        },
-        {
-          ResourceType = "collection"
-          Resource     = ["collection/${lower(var.project)}-collection"]
-          Permission = [
-            "aoss:CreateCollectionItems",
-            "aoss:UpdateCollectionItems",
-            "aoss:DescribeCollectionItems"
-          ]
-        }
-      ]
-      Principal = [aws_iam_role.knowledge_base.arn]
-    }
-  ])
-
+  # Đảm bảo Collection được tạo trước khi áp dụng Access Policy
   depends_on = [
-    aws_iam_role.knowledge_base,
-    aws_opensearchserverless_collection.rag
+    aws_opensearchserverless_collection.rag_collection,
+    aws_iam_role.kb_service_role
   ]
 }
