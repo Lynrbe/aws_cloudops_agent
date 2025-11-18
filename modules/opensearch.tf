979c5fd15965716bd5bd3e42532a1e66bd0f7bdf
FIXED-TERM Doan Thi Khanh Linh (MS/EDS32-XC) <fixed-term.Linh.DoanThiKhanh@vn.bosch.com>
2025-11-18 10:25:23 +0700

    update opensearch template #1

--- modules/opensearch.tf
+++ modules/opensearch.tf
@@ -1,39 +1,91 @@
-resource "aws_opensearchserverless_security_policy" "encryption_policy" {
-  name = "${var.project}-kb-encryption-policy"
+# Encryption Security Policy
+resource "aws_opensearchserverless_security_policy" "encryption" {
+  name = "${lower(var.project)}-encryption-policy"
   type = "encryption"
-  
+
   policy = jsonencode({
-    "Rules" : [
+    Rules = [
       {
-        "ResourceType" : "collection",
-        "Resource" : ["collection/${aws_opensearchserverless_collection.rag_collection.name}"]
+        ResourceType = "collection"
+        Resource     = ["collection/${lower(var.project)}-collection"]
       }
-    ],
-    "AWSOwnedKey" : true 
+    ]
+    AWSOwnedKey = true
   })
 }
 
-resource "aws_opensearchserverless_security_policy" "network_policy" {
-  name = "${var.project}-kb-network-policy"
+# Network Security Policy
+resource "aws_opensearchserverless_security_policy" "network" {
+  name = "${lower(var.project)}-network-policy"
   type = "network"
-  
+
   policy = jsonencode([
     {
-      "Rules" : [
+      Rules = [
         {
-          "ResourceType" : "collection",
-          "Resource" : ["collection/${aws_opensearchserverless_collection.rag_collection.name}"]
+          ResourceType = "collection"
+          Resource     = ["collection/${lower(var.project)}-collection"]
         }
-      ],
-      "AllowFromPublic" : true 
+      ]
+      AllowFromPublic = true
     }
   ])
 }
 
 # OpenSearch Serverless Collection
-resource "aws_opensearchserverless_collection" "rag_collection" {
-  name = "${var.project}-kb-collection" 
+resource "aws_opensearchserverless_collection" "rag" {
+  name = "${lower(var.project)}-collection"
   type = "VECTORSEARCH"
+
+  depends_on = [
+    aws_opensearchserverless_security_policy.encryption,
+    aws_opensearchserverless_security_policy.network
+  ]
+
+  tags = {
+    Name    = "${var.project}-Collection"
+    Project = var.project
+  }
+}
+
+# Data Access Policy
+resource "aws_opensearchserverless_access_policy" "data_access" {
+  name = "${lower(var.project)}-data-access-policy"
+  type = "data"
+
+  policy = jsonencode([
+    {
+      Rules = [
+        {
+          ResourceType = "index"
+          Resource     = ["index/${lower(var.project)}-collection/*"]
+          Permission = [
+            "aoss:CreateIndex",
+            "aoss:DescribeIndex",
+            "aoss:UpdateIndex",
+            "aoss:DeleteIndex",
+            "aoss:ReadDocument",
+            "aoss:WriteDocument"
+          ]
+        },
+        {
+          ResourceType = "collection"
+          Resource     = ["collection/${lower(var.project)}-collection"]
+          Permission = [
+            "aoss:CreateCollectionItems",
+            "aoss:UpdateCollectionItems",
+            "aoss:DescribeCollectionItems"
+          ]
+        }
+      ]
+      Principal = [aws_iam_role.knowledge_base.arn]
+    }
+  ])
+
+  depends_on = [
+    aws_iam_role.knowledge_base,
+    aws_opensearchserverless_collection.rag
+  ]
 }
 
 # Access Policy for Bedrock Knowledge Base Role 
