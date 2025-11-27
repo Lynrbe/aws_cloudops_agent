c9ad21bd4e6db76d479dd29edb4d30d0a3d1b70e
Lynrbe <khanhlinh26004@gmail.com>
2025-11-28 01:57:19 +0700

    update region of lambda ingest

--- modules/lambda_ingest.tf
+++ modules/lambda_ingest.tf
@@ -1,5 +1,8 @@
 # Lambda Ingestion (Sử dụng Artifact ZIP từ GitHub Actions)
+# MUST be in same region as S3 Documents bucket (ap-southeast-2) for S3 event trigger
 resource "aws_lambda_function" "ingest" {
+  provider = aws.bedrock  # Deploy to ap-southeast-2 (same as S3 Documents and Knowledge Base)
+
   function_name = "${var.project}-ingest"
   role          = aws_iam_role.lambda_exec.arn
   handler       = "lambda_handler.lambda_handler"
@@ -7,15 +10,15 @@ resource "aws_lambda_function" "ingest" {
 
   s3_bucket = data.aws_s3_bucket.rag_artifacts.bucket
   # Key được truyền từ GitHub Actions
-  s3_key    = var.ingest_artifact_key 
+  s3_key    = var.ingest_artifact_key
 
-  timeout     = 30 
+  timeout     = 30
   memory_size = 512
 
   environment {
     variables = {
       KNOWLEDGE_BASE_ID = aws_bedrockagent_knowledge_base.kb.id
-      REGION = var.bedrock_region
+      REGION            = var.bedrock_region
     }
   }
 }
--- modules/s3_event_trigger.tf
+++ modules/s3_event_trigger.tf
@@ -1,22 +1,23 @@
 # S3 Event Notification to trigger Lambda Ingestion automatically
 # when new files are uploaded to docs/ prefix
 
-# IMPORTANT: Lambda permission must be in same region as Lambda (ap-southeast-1)
-# But it can reference S3 bucket in different region (ap-southeast-2)
+# IMPORTANT: Both Lambda and S3 must be in SAME region (ap-southeast-2)
+# Lambda permission must be in same region as Lambda function
 resource "aws_lambda_permission" "allow_s3_invoke" {
-  # No provider = uses default region (ap-southeast-1, same as Lambda)
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.ingest.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.rag_documents.arn
+  provider = aws.bedrock  # Same as Lambda Ingest (ap-southeast-2)
+
+  statement_id   = "AllowS3Invoke"
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.ingest.function_name
+  principal      = "s3.amazonaws.com"
+  source_arn     = aws_s3_bucket.rag_documents.arn
   source_account = data.aws_caller_identity.current.account_id
 }
 
 # S3 Bucket Notification - must be in same region as S3 bucket (ap-southeast-2)
 resource "aws_s3_bucket_notification" "document_upload_trigger" {
   provider = aws.bedrock  # S3 bucket is in ap-southeast-2
-  bucket = aws_s3_bucket.rag_documents.id
+  bucket   = aws_s3_bucket.rag_documents.id
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.ingest.arn
